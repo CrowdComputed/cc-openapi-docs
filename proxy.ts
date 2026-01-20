2e538982a59f860e588590aea0cd9d2b71866081
noah <yangchen.xiyou@gmail.com>
2026-01-20 23:10:48 +0800

    feat: add missing headers to proxy middleware

--- proxy.ts
+++ proxy.ts
@@ -1,26 +1,14 @@
 import { createI18nMiddleware } from "fumadocs-core/i18n/middleware";
-import type { NextFetchEvent, NextRequest } from "next/server";
-import { NextResponse } from "next/server";
 import { i18n } from "./src/lib/i18n";
 
-const i18nMiddleware = createI18nMiddleware(i18n);
+export default createI18nMiddleware(i18n);
 
 export const config = {
+  // Matcher ignoring `/_next/` and `/api/`
+  // You may need to adjust it to ignore static assets in `/public` folder
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  missing: [
+    { type: "header", key: "next-router-prefetch" },
+    { type: "header", key: "purpose", value: "prefetch" },
+  ],
 };
-
-export async function proxy(request: NextRequest, event: NextFetchEvent) {
-  let response = await i18nMiddleware(request, event);
-
-  if (!response) {
-    response = NextResponse.next();
-  }
-
-  const cspValue =
-    "default-src 'self' http: https: ws: wss: data: blob: 'unsafe-inline'; frame-ancestors *;";
-
-  response.headers.delete("Content-Security-Policy");
-  response.headers.set("Content-Security-Policy", cspValue);
-
-  return response;
-}
